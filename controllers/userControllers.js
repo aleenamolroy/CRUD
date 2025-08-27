94941ff5055ca4ec3f89c9c4bac6be23f635352a
Aleenamol Roy <aleenamolroy2025@mca.ajce.in>
2025-08-27 15:36:10 +0530

    commit after error handling

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,20 +1,47 @@
 import { ObjectId } from 'mongodb'
-import {connectDB,UserCollection} from '../db/db.js'
-export async function addUser(req,res){
-   const result= await UserCollection.insertOne(req.body)
-   res.send(result)
+import { connectDB, UserCollection } from '../db/db.js'
+export async function addUser(req, res) {
+    try {
+        const result = await UserCollection.insertOne(req.body)
+        res.send(result)
+    }
+    catch {
+        res.sendStatus(500)
+    }
 }
-export async function getusers(req,res){
-    const result=await UserCollection.find().toArray()
+export async function getusers(req, res) {
+    try {
+        const result = await UserCollection.find().toArray()
+        res.send(result)
+    }
+    catch {
+        res.sendStatus(500)
+
+    }
+}
+export async function UpdateUser(req, res) {
+    try {
+        const userId = req.params.id;
+        const result = await UserCollection.deleteOne({ _id: new ObjectId(userId) })
+        res.send(result)
+    }
+    catch {
+        res.sendStatus(500)
+
+    }
+    const userId = req.params.id;
+    const result = await UserCollection.updateOne({ _id: new ObjectId(userId) }, { $set: req.body })
     res.send(result)
 }
-export async function UpdateUser(req,res) {
-    const userId= req.params.id;
-    const result= await UserCollection.updateOne({_id:new ObjectId(userId)},{$set:req.body})
-    res.send(result)
-}
-export async function DeleteUser(req,res){
-    const userId=req.params.id;
-    const result= await UserCollection.deleteOne({_id: new ObjectId(userId)})
-    res.send(result)
+export async function DeleteUser(req, res) {
+    try {
+        const userId = req.params.id;
+        const result = await UserCollection.deleteOne({ _id: new ObjectId(userId) })
+        res.send(result)
+    }
+    catch {
+        res.sendStatus(500)
+
+    }
+
 }
